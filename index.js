e230c446419d3c7070d496885d35e9b5b63977d6
James Royer <walkingalchemy@gmail.com>
2017-09-24 02:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function shout(string) {
   return string.toUpperCase()
-}
+};
 
 function whisper(string) {
   return string.toLowerCase()
-}
+};
 
 function logShout(string) {
   console.log(shout(string))
-}
+};
 
 function logWhisper(string) {
   console.log(whisper(string))
-}
+};
 
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
@@ -22,4 +22,4 @@ function sayHiToGrandma(string) {
   } elseif (string === "I love you, Grandma.") {
     return "I love you, too."
   }
-}
+};
